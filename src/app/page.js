d80313ca1d3b7ae913ff3545c5646c2b99b77833
Manas Malla <manasmalla.dev@gmail.com>
2024-03-02 00:04:22 +0530

    added initial landing
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,217 @@
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
 
 export default function Home() {
+  useGSAP(() => {
+    const tl = gsap
+      .timeline({})
+      // .fromTo(
+      //   ".loader",
+      //   { y: 0 },
+      //   { ease: "Power4.easeOut", y: "-100%", duration: 1 },
+      //   "+=1"
+      // )
+      .fromTo(
+        ".text-inside",
+        { translateY: 400 },
+        {
+          ease: "Power4.easeOut",
+          translateY: 0,
+          stagger: 0.13,
+          duration: 2,
+        }
+      );
+    // .fromTo(
+    //   ".word",
+    //   { y: 40 },
+    //   {
+    //     ease: "Power4.easeOut",
+    //     y: 0,
+    //     stagger: 0.13,
+    //     duration: 2,
+    //   }
+    // );
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      {/* <div class="z-2 absolute w-screen h-screen bg-black loader items-center flex justify-center">
+        <img src="radhasundariyam.png" class="w-1/4" />
+      </div> */}
+      <nav className="p-6 fixed items-center justify-between flex w-full">
+        <div class="hidden lg:block  relative">
+          <button class="hamburger flex cursor-pointer rounded-full">
+            <svg
+              class=""
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4 15H20"
+                stroke="black"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M4 8H20"
+                stroke="black"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+            </svg>
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          title="go to homepage"
+          className=" h-12 w-12 left-0 right-0  ml-1 lg:ml-auto block mt-[0.375rem] lg:absolute  m-auto -top-10 bottom-0 lg:mt-auto -translate-y-1/2 lg:translate-y-0 -translate-x-1/2"
+          href="/"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+          <button class="lottie-container  ">
+            <div
+              autoplay=""
+              className="scale-110 items-center flex justify-center"
+              style={{ width: "5.5rem", height: "5.5rem" }}
+            >
+              <img src="logo.svg" className="h-[56%]" />
+            </div>
+          </button>
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+        <div className="flex items-center gap-x-2">
+          <div className="btn btn-white !w-fit">
+            <span className="flex w-fit">Contact Us</span>
+          </div>
+          <div className="btn btn-black">Submit your project</div>
+        </div>
+      </nav>
+      <div class="h-[5.3125rem]  lg:h-[6.875rem]"></div>
+      <div className="landing pt-6">
+        <div className="px-12">
+          <h2 className="main-text mb-12">
+            <div className="flex flex-wrap">
+              <div
+                className="word inline- flex items-center mr-4"
+                style={{
+                  translate: "none",
+                  rotate: "none",
+                  scale: "none",
+                  transform: "translate(0px)",
+                }}
+              >
+                <div className="h-[130%] flex items-center overflow-hidden">
+                  <span
+                    className="relative text-inside will-change-auto"
+                    style={{
+                      translate: "none",
+                      rotate: "none",
+                      scale: "none",
+                      transform: "translate(0px)",
+                    }}
+                  >
+                    Transform
+                  </span>
+                </div>
+              </div>
+              <div
+                className="word inline- flex items-center mr-4"
+                style={{
+                  translate: "none",
+                  rotate: "none",
+                  scale: "none",
+                  transform: "translate(0px)",
+                }}
+              >
+                <div className="h-[130%] flex items-center overflow-hidden">
+                  <span
+                    className="relative text-inside will-change-auto"
+                    style={{
+                      translate: "none",
+                      rotate: "none",
+                      scale: "none",
+                      transform: "translate(0px)",
+                    }}
+                  >
+                    your
+                  </span>
+                </div>
+              </div>
+              <div
+                className="word inline- flex items-center mr-4"
+                style={{
+                  translate: "none",
+                  rotate: "none",
+                  scale: "none",
+                  transform: "translate(0px)",
+                }}
+              >
+                <div className="h-[130%] flex items-center overflow-hidden">
+                  <span
+                    className="relative text-inside will-change-auto"
+                    style={{
+                      translate: "none",
+                      rotate: "none",
+                      scale: "none",
+                      transform: "translate(0px)",
+                    }}
+                  >
+                    vision
+                  </span>
+                </div>
+              </div>
+              <div
+                className="word inline- flex items-center mr-4"
+                style={{
+                  translate: "none",
+                  rotate: "none",
+                  scale: "none",
+                  transform: "translate(0px)",
+                }}
+              >
+                <div className="h-[130%] flex items-center overflow-hidden">
+                  <span
+                    className="relative text-inside will-change-auto"
+                    style={{
+                      translate: "none",
+                      rotate: "none",
+                      scale: "none",
+                      transform: "translate(0px)",
+                    }}
+                  >
+                    into
+                  </span>
+                </div>
+              </div>
+              <div
+                className="word inline- flex items-center mr-4"
+                style={{
+                  translate: "none",
+                  rotate: "none",
+                  scale: "none",
+                  transform: "translate(0px)",
+                }}
+              >
+                <div className="h-[130%] flex items-center overflow-hidden">
+                  <span
+                    className="relative text-inside will-change-auto"
+                    style={{
+                      translate: "none",
+                      rotate: "none",
+                      scale: "none",
+                      transform: "translate(0px)",
+                    }}
+                  >
+                    reality!
+                  </span>
+                </div>
+              </div>
+            </div>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
       </div>
     </main>
   );
