24352347f33f8599fe5ef42a1abb15f09c84432f
Manas Malla <manasmalla.dev@gmail.com>
2024-03-06 19:06:49 +0530

    added `projects` fold
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,13 @@ import { Navigation } from "swiper/modules";
 
 import Swiper from "swiper";
 import "swiper/css";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import LandingHero from "./components/landing/hero";
 import AnantaNavigationBar from "./components/ananta_navigation_bar";
 import SliderSwiper from "./components/slider_swiper";
 
 export default function Home() {
+  let el = useRef();
   useEffect(() => {
     // init Swiper:
     /*
@@ -82,19 +83,38 @@ export default function Home() {
           duration: 2,
         }
       );
-    const t2 = gsap.globalTimeline.fromTo(
-      ".fold",
-      { y: 200 },
-      {
-        y: 0,
-        duration: 1,
-        scrollTrigger: {
-          toggleActions: "play none none reverse",
-          trigger: ".fold",
-          start: "top 95%",
-        },
-      }
-    );
+    // let p8 = gsap.utils.selector();
+    Array.from(document.querySelectorAll(".fold")).map((element) => {
+      return gsap.fromTo(
+        element,
+        { y: 200 },
+        {
+          y: 0,
+          duration: 1,
+          scrollTrigger: {
+            toggleActions: "play none none reverse",
+            trigger: element,
+            start: "top 95%",
+          },
+        }
+      );
+    });
+    // p8(".fold").map((element) => console.log(element));
+    // p8(".fold").map((element) =>
+    // gsap.fromTo(
+    //   ".fold",
+    //   { y: 200 },
+    //   {
+    //     y: 0,
+    //     duration: 1,
+    //     scrollTrigger: {
+    //       toggleActions: "play none none reverse",
+    //       trigger: ".fold",
+    //       start: "top 95%",
+    //     },
+    //   }
+    // );
+    // );
   });
   return (
     <main className="">
@@ -122,14 +142,14 @@ export default function Home() {
                 </div>
                 <a
                   class="!hidden lg:!block lg:mt-auto"
-                  href="https://dribbble.com/piqostudio"
+                  href="https://dribbble.com/theananta"
                 >
                   <div class="flex !gap-4 drible-btn  style_container__ZhG39">
                     <span class="initial items-center z-[2] flex gap-2 ">
                       <div autoplay="" class=" w-8">
                         <img src="dribble.svg" />
                       </div>
-                      Piqo’s Dribbble
+                      Ananta's Dribbble
                     </span>
                     <span
                       aria-hidden="true"
@@ -138,7 +158,7 @@ export default function Home() {
                       <div autoplay="" class=" w-8">
                         <img src="dribble.svg" />
                       </div>
-                      Piqo’s Dribbble
+                      Ananta's Dribbble
                     </span>
                     <span class="flex !gap-4 drible-btn  !mb-0 bg-box absolute !w-full h-full"></span>
                   </div>
@@ -293,6 +313,254 @@ export default function Home() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="fold">
+          <div class="styles_container__IpxrY pad_layout">
+            <div class="section-title">projects</div>
+            <div class="flex justify-between -mt-4 items-end mb-6 lg:mb-12">
+              <div class="big-title">Lorem Ipsum Dolor Amet Contesteur</div>
+              <a class="!hidden lg:!block lg:mt-auto" href="/products">
+                <div class="flex !gap-4 more-btn  style_container__ZhG39">
+                  <span class="initial items-center z-[2] flex gap-2 items-center">
+                    More Assets
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8.34106 6.02149L18 6L17.9796 15.6601"
+                        stroke="black"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></path>
+                      <path
+                        d="M18 6.00195L6.0004 18.0004"
+                        stroke="black"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span
+                    aria-hidden="true"
+                    class="secondary  h-full z-[2] flex gap-2 items-center"
+                  >
+                    More Assets
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8.34106 6.02149L18 6L17.9796 15.6601"
+                        stroke="black"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></path>
+                      <path
+                        d="M18 6.00195L6.0004 18.0004"
+                        stroke="black"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span class="flex !gap-4 more-btn  !mb-0 bg-box absolute !w-full h-full"></span>
+                </div>
+              </a>
+            </div>
+            <div class="flex-col flex lg:grid lg:grid-cols-3 gap-8 ">
+              <a href="https://iconly.pro">
+                <div class="styles_product___o3HF">
+                  <div class="img-container mb-4">
+                    <img src="stemquest.png" alt="" />
+                    <div class="more">
+                      <span class="more-text">Buy now</span>
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M8.3418 6.02155L18.0007 6.00006L17.9803 15.6602"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></path>
+                        <path
+                          d="M17.9996 6.00153L6 18"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div class="title-line mb-3 flex w-full justify-between items-center">
+                    <h6 class="product-title">STEMQuest</h6>
+                    <div class="price">₹1500</div>
+                  </div>
+                  <div class="product-text">
+                    Discover | Learn | Create
+                    <br />
+                    Help your children learn with engaging STEM-based
+                    educational kits
+                  </div>
+                </div>
+              </a>
+              <a href="https://piqodesign.gumroad.com/l/iconly3d">
+                <div class="styles_product___o3HF">
+                  <div class="img-container mb-4">
+                    <img src="autosmith.png" alt="AutoSmith Image" />
+                    <div class="more">
+                      <span class="more-text">Buy now</span>
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M8.3418 6.02155L18.0007 6.00006L17.9803 15.6602"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></path>
+                        <path
+                          d="M17.9996 6.00153L6 18"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div class="title-line mb-3 flex w-full justify-between items-center">
+                    <h6 class="product-title">AutoSmith</h6>
+                    <div class="price">₹50</div>
+                  </div>
+                  <div class="product-text">
+                    Connects nearby partnered local mechanics offering on-road
+                    service.
+                  </div>
+                </div>
+              </a>
+              <a href="https://piqodesign.gumroad.com/l/DeliveryIllustration">
+                <div class="styles_product___o3HF">
+                  <div class="img-container mb-4">
+                    <img src="bakery.png" alt="" />
+                    <div class="more">
+                      <span class="more-text">Buy now</span>
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M8.3418 6.02155L18.0007 6.00006L17.9803 15.6602"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></path>
+                        <path
+                          d="M17.9996 6.00153L6 18"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div class="title-line mb-3 flex w-full justify-between items-center">
+                    <h6 class="product-title">Hand Baked</h6>
+                    <div class="price">₹200</div>
+                  </div>
+                  <div class="product-text">
+                    Unlock Your Shipping and Delivery Experience with 120 Vector
+                    Illustrations
+                  </div>
+                </div>
+              </a>
+            </div>
+            <a class="!block mt-8 lg:!hidden" href="/products">
+              <div class="flex !gap-4 more-btn  style_container__ZhG39">
+                <span class="initial items-center z-[2] flex gap-2 items-center">
+                  More Assets
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8.34106 6.02149L18 6L17.9796 15.6601"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                    <path
+                      d="M18 6.00195L6.0004 18.0004"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                  </svg>
+                </span>
+                <span
+                  aria-hidden="true"
+                  class="secondary  h-full z-[2] flex gap-2 items-center"
+                >
+                  More Assets
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8.34106 6.02149L18 6L17.9796 15.6601"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                    <path
+                      d="M18 6.00195L6.0004 18.0004"
+                      stroke="black"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                  </svg>
+                </span>
+                <span class="flex !gap-4 more-btn  !mb-0 bg-box absolute !w-full h-full"></span>
+              </div>
+            </a>
           </div>
         </div>
       </div>
