407e975d1bd74b9d33ade076066c7d6bc92469f6
Manas Malla <manasmalla.dev@gmail.com>
2024-03-12 10:30:00 +0530

    commented the works section
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import { Navigation } from "swiper/modules";
 
 import Swiper from "swiper";
 import "swiper/css";
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
 import LandingHero from "./components/landing/hero";
 import AnantaNavigationBar from "./components/ananta_navigation_bar";
 import SliderSwiper from "./components/slider_swiper";
@@ -19,57 +19,57 @@ import OurClients from "./components/our-clients";
 import OurPricing from "./components/our-pricing";
 
 export default function Home() {
-  let el = useRef();
-  useEffect(() => {
-    // init Swiper:
-    /*
-    allowTouchMove: !0,
-              loop: !0,
-              centeredSlidesBounds: !0,
-              centeredSlides: !0,
-              freeMode: !0,
-              spaceBetween: 16,
-              speed: 1e3,
-              navigation: { nextEl: "#nextBtnslide", prevEl: "#prevBtnslide" },
-              slidesPerView: 1.3,
-              initialSlide: 2,
-              breakpoints: {
-                440: { spaceBetween: 20, slidesPerView: 1.8 },
-                1024: {
-                  centeredSlidesBounds: !1,
-                  centeredSlides: !1,
-                  slidesPerView: "auto",
-                  spaceBetween: 32,
-                },
-              },
-    */
-    const swiper = new Swiper(".swiper", {
-      allowTouchMove: true,
-      loop: true,
-      centeredSlides: true,
-      centeredSlidesBounds: true,
-      freeMode: true,
-      spaceBetween: 16,
-      speed: 1000,
-      modules: [Navigation],
-      slidesPerView: 1.3,
-      initialSlide: 2,
+  // let el = useRef();
+  // useEffect(() => {
+  //   // init Swiper:
+  //   /*
+  //   allowTouchMove: !0,
+  //             loop: !0,
+  //             centeredSlidesBounds: !0,
+  //             centeredSlides: !0,
+  //             freeMode: !0,
+  //             spaceBetween: 16,
+  //             speed: 1e3,
+  //             navigation: { nextEl: "#nextBtnslide", prevEl: "#prevBtnslide" },
+  //             slidesPerView: 1.3,
+  //             initialSlide: 2,
+  //             breakpoints: {
+  //               440: { spaceBetween: 20, slidesPerView: 1.8 },
+  //               1024: {
+  //                 centeredSlidesBounds: !1,
+  //                 centeredSlides: !1,
+  //                 slidesPerView: "auto",
+  //                 spaceBetween: 32,
+  //               },
+  //             },
+  //   */
+  //   const swiper = new Swiper(".swiper", {
+  //     allowTouchMove: true,
+  //     loop: true,
+  //     centeredSlides: true,
+  //     centeredSlidesBounds: true,
+  //     freeMode: true,
+  //     spaceBetween: 16,
+  //     speed: 1000,
+  //     modules: [Navigation],
+  //     slidesPerView: 1.3,
+  //     initialSlide: 2,
 
-      breakpoints: {
-        440: { spaceBetween: 20, slidesPerView: 1.8 },
-        1024: {
-          centeredSlidesBounds: false,
-          centeredSlides: false,
-          slidesPerView: "auto",
-          spaceBetween: 32,
-        },
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-  });
+  //     breakpoints: {
+  //       440: { spaceBetween: 20, slidesPerView: 1.8 },
+  //       1024: {
+  //         centeredSlidesBounds: false,
+  //         centeredSlides: false,
+  //         slidesPerView: "auto",
+  //         spaceBetween: 32,
+  //       },
+  //     },
+  //     navigation: {
+  //       nextEl: ".swiper-button-next",
+  //       prevEl: ".swiper-button-prev",
+  //     },
+  //   });
+  // });
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger);
     gsap.globalTimeline.fromTo(
@@ -141,7 +141,7 @@ export default function Home() {
             <AnantaDribbble />
           </div>
           <div className="relative mt-6 lg:mt-12">
-            <div class="swiper">
+            {/* <div class="swiper">
               <div class="swiper-wrapper">
                 <SliderSwiper imageSrc="bheeshma.png" />
                 <SliderSwiper imageSrc="reema-holidays.png" />
@@ -153,7 +153,7 @@ export default function Home() {
                 <SliderSwiper imageSrc="bheeshma.png" />
                 <SliderSwiper imageSrc="bheeshma.png" />
               </div>
-            </div>
+            </div> */}
             {/* <div className="absolute items-center justify-between z-[5] flex h-full top-0 w-full px-11">
             <div class="swiper-button-prev bg-black cursor-pointer w-[5.5rem] h-[5.5rem] rounded-full items-center justify-center flex text-white group-hover:scale-100 scale-0 transition duration-300 -translate-x-[150%] group-hover:translate-x-0">
               Back
